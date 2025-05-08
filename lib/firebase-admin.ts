0f5f94e132df84e0cd51749a7b4fdc88a5c34ef9
Matthew Febbraro <fuelform.online@gmail.com>
2025-05-08 00:50:22 -0400

    FuelForm Updates

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,7 +4,7 @@ import * as admin from 'firebase-admin';
 export function getAdminApp() {
   if (!admin.apps.length) {
 
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\n/g, '\n');
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
     admin.initializeApp({
       credential: admin.credential.cert({
