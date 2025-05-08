9a617e1fcd392d326dbcb93c1867f5beb6721a6e
Matthew Febbraro <fuelform.online@gmail.com>
2025-05-08 00:39:11 -0400

    FuelForm Updates

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -3,16 +3,17 @@ import * as admin from 'firebase-admin';
 
 export function getAdminApp() {
   if (!admin.apps.length) {
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+    
 
     admin.initializeApp({
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        
       }),
     });
   }
-
+  console.log(process.env.FIREBASE_PRIVATE_KEY?.split('\n')[0]);
   return admin;
 }
